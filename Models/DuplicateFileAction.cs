e15448f134c4e430697cde3a9a910315c9b3169f
Michel Santos <108155503+mps155@users.noreply.github.com>
2026-05-24 21:54:09 -0300

    Add files via upload

---
+++ Models/DuplicateFileAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamCleaner.Models
+{
+    public class DuplicateFileAction
+    {
+        public string FilePath { get; set; } = string.Empty;
+        public DateTime LastModified { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ Models/ScannerSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamCleaner.Models
+{
+    public class ScannerSettings
+    {
+        public int RunIntervalMinutes { get; set; }
+        public string[] TargetDirectories { get; set; } = [];
+        public string[] AllowedExtensions { get; set; } = [];
+        public string OutputPath { get; set; } = string.Empty;
+        public bool RemoveOlderFiles { get; set; }
+        public string MoveToBackupFolder { get; set; } = string.Empty;
+    }
+}
